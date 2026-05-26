b455b26c577aabfb414f5f005dec0859c0dff0de
yang0w0 <166664111992@qq.com>
2026-05-26 19:37:09 +0800

    地图交互补充

--- src/tilesets/Gemini_Generated_Image_a0qfh7a0qfh7a0qf.tsx
+++ src/tilesets/Gemini_Generated_Image_a0qfh7a0qfh7a0qf.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="Gemini_Generated_Image_a0qfh7a0qfh7a0qf" tilewidth="16" tileheight="16" tilecount="16512" columns="172">
- <image source="../../Downloads/Gemini_Generated_Image_a0qfh7a0qfh7a0qf.png" width="2754" height="1536"/>
+ <image source="Gemini_Generated_Image_a0qfh7a0qfh7a0qf.png" width="2752" height="1536"/>
 </tileset>
